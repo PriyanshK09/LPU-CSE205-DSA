43ee79e8b7a21936ba1ea21c7812bb179e39f14e
PriyanshK09 <priyanshkhare4905@gmail.com>
2023-08-28 17:35:21 +0530

    Code corrected

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,65 +1,58 @@
-// Local Variables are created in stack and are destroyed when the function is over
-// Dynamic Variables are created in heap and are destroyed when the program is over
-
-
 #include<iostream>
 using namespace std;
-class node
-{
-    public:
-    int data;
-    node *link;
+class node{
+public:
+int data;
+node *link;
 };
-class Linkedlist
-{
-    node *head;
-    public:
-    Linkedlist() //constructor function
-    {
-      head=NULL;
-    }
-    void create();
-    void display();
-
+class linkedlist{
+public:
+node *head;
+linkedlist(){
+head=NULL;
+}
+void create();
+void display();
 };
-void Linkedlist::create()
-{
-    int n;
-    node *temp=new node;
-    cout<<"Enter the data to insert into node: ";
-    cin>>n;
-    temp->data=n;
-    temp->link=NULL;
-    if(head==NULL)
-    {
-        head=temp;
-    }
-    else
-    {
-        node *t=head;
-        while(t->link!=NULL)
-        {
-            t=t->link;
-        }
-        t->link=temp;
-    }
-
-};
-void Linkedlist::display()
-{
-    node *t=head;
-    while(t!=NULL)
-    {
-        cout<<t->data<<"=>";
-        t=t->link;
-    }
-    cout<<"NULL";
-};
-int main()
-{
-    Linkedlist l;
-    l.create();
-    l.create();
-    l.display(); // 1=>2=>3=>NULL : Expected Output
-    return 0;
+void linkedlist::create(){
+int n;
+node *temp=new node;
+cout<<"Enter the data insert into node: ";
+cin>>n;
+temp->data=n;
+temp->link=NULL;
+if(head==NULL){
+head=temp;
+}
+else{
+node *t=head;
+while(t->link!=NULL){
+t=t->link;
+}
+t->link=temp;
+}
+}
+void linkedlist::display(){
+node *t=head;
+while(t!=NULL){
+if(t->link!=NULL){
+cout<<t->data<<" ==> ";
+}
+else{
+cout<<t->data;
+}
+t=t->link;
+}
+}
+int main(){
+linkedlist l1;
+int n;
+cout<<"Enter the number of elements you want to enter: ";
+cin>>n;
+for(int i=0;i<n;i++){
+l1.create();
+}
+cout<<"Your created list is : ";
+l1.display();
+return 0;
 }
